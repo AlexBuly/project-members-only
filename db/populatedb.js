afe74127f4b23116b88bced5b37bfc465600f2bc
Alex Buly <asbuly@icloud.com>
2025-11-19 20:02:37 -0500

    Add database populate

---
+++ db/populatedb.js
@@ -0,0 +1,37 @@
+require("dotenv").config({ override: true });
+const pool = require("./pool");
+
+async function populate() {
+    try {
+        await pool.query(
+            `
+                CREATE TABLE IF NOT EXISTS Users (
+                    id SERIAL PRIMARY KEY,
+                    first_name TEXT NOT NULL,
+                    last_name TEXT NOT NULL,
+                    username VARCHAR(255) NOT NULL,
+                    password VARCHAR(50) NOT NULL,
+                    member BOOLEAN DEFAULT FALSE
+                );
+            `
+        );
+        await pool.query (
+            `
+                CREATE TABLE Messages (
+                    msg_id SERIAL PRIMARY KEY,
+                    title VARCHAR(255),
+                    text VARCHAR(255),
+                    user_id INT,
+                    FOREIGN KEY (user_id) REFERENCES Users(id)
+                );
+            `
+        );
+    } catch (err) {
+        console.error("Error populating DB:", err);
+    } finally {
+        await pool.end();
+        console.log("Database populated successfully!");
+    }
+}
+
+populate();
