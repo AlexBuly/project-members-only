dba90f618ae2fab40202f911f314559c224f7c8e
Alex Buly <asbuly@icloud.com>
2025-11-22 20:37:31 -0500

    Add validation

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,8 +1,17 @@
 const pool = require("../db/pool");
 const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 const addNewUser = async (req, res, next) => {
+    const errors = validationResult(req);
     const {first_name, last_name, username, password} = req.body;
+
+     if (!errors.isEmpty()) {
+        return res.status(400).render("sign-up-form", {
+            title: "Sign Up",
+            errors: errors.array(),
+        });
+    }
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -11,13 +20,17 @@ const addNewUser = async (req, res, next) => {
             "INSERT INTO Users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4)",
             [first_name, last_name, username, hashedPassword]
         );
+        res.redirect("/login");
     } catch (err) {
         next(err);
     }
 }
 
-const renderSignUpForm = async (req, res) => {
-    res.render("sign-up-form", {title: "Sign Up"})
+const renderSignUpForm = (req, res) => {
+    res.render("sign-up-form", {
+        title: "Sign Up",
+        errors: []
+    })
 };
 
 module.exports = {
--- routes/signupRouter.js
+++ routes/signupRouter.js
@@ -1,8 +1,21 @@
 const { Router } = require("express");
 const router = Router();
 const signupController = require("../controllers/signupController");
+const { body } = require("express-validator");
 
 router.get("/", signupController.renderSignUpForm);
-router.post("/", signupController.addNewUser);
+router.post("/", 
+    [
+        body("password")
+            .isLength({min: 5}).withMessage("Password must be at least 5 characters long"),
+
+        body("confirmPassword")
+            .custom((value, {req}) => {
+                if (value !== req.body.password) {
+                    throw new Error("Passwords do not match");
+                }
+                return true;
+            })
+    ], signupController.addNewUser);
 
 module.exports = router;
