7bff3e66a98d6a0a69c0d50eacecba1958822e50
Alex Buly <asbuly@icloud.com>
2025-11-22 20:35:20 -0500

    Move authentication info

--- app.js
+++ app.js
@@ -1,16 +1,65 @@
 const express = require("express");
 const path = require("path");
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const pool = require("./db/pool");
+const bcrypt = require("bcryptjs");
 const app = express();
 app.set("view engine", "ejs");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 const signupRouter = require("./routes/signupRouter");
 const loginRouter = require("./routes/loginRouter");
 const indexRouter = require("./routes/indexRouter");
+
+app.use(
+    session({
+        secret: "cats",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const { rows } = await pool.query("SELECT * FROM Users WHERE username = $1", [username]);
+            const user = rows[0];
+
+            if (!user) return done(null, false, { message: "Incorrect username" });
+
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) return done(null, false, { message: "Incorrect password" });
+
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const { rows } = await pool.query("SELECT * FROM Users WHERE id = $1", [id]);
+        done(null, rows[0]);
+    } catch (err) {
+        done(err);
+    }
+});
+
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
 
 app.use("/sign-up", signupRouter);
 app.use("/login", loginRouter);
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,87 +1,25 @@
-const pool = require("../db/pool");
-const bcrypt = require("bcryptjs");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const app = express();
 
 const getUser = (req, res) => {
-    res.render("log-in-form", {title: "Login"})
+    res.render("log-in-form", { title: "Login" });
 };
 
-const loginUser = (req, res) => {
-    app.use(
-        session({
-            secret: "cats",
-            resave: false,
-            saveUninitialized: false,
-        })
-    );
-
-    app.use(passport.initialize());   // REQUIRED
-    app.use(passport.session());   
-
-    passport.use(
-        new LocalStrategy(async (username, password, done) => {
-            try {
-            const { rows } = await pool.query(
-                "SELECT * FROM Users WHERE username = $1",
-                [username]
-            );
-            const user = rows[0];
-
-            if (!user) {
-                return done(null, false, { message: "Incorrect username" });
-            }
-
-            // Compare hashed password
-            const match = await bcrypt.compare(password, user.password);
-            if (!match) {
-                return done(null, false, { message: "Incorrect password" });
-            }
-
-            return done(null, user);
-            } catch (err) {
-            return done(err);
-            }
-        })
-    );
-
-    passport.serializeUser((user, done) => {
-        done(null, user.id);
-    });
-
-    passport.deserializeUser(async (id, done) => {
-        try {
-            const { rows } = await pool.query(
-                "SELECT * FROM users WHERE id = $1",
-                [id]
-            );
-            done(null, rows[0]);
-        } catch (err) {
-            done(err);
-        }
-    });
-
-    app.use((req, res, next) => {
-        res.locals.user = req.user;
-        next();
-    });
-
+const loginUser = (req, res, next) => {
     passport.authenticate("local", {
         successRedirect: "/",
-        failureRedirect: "/log-in",
-    })
-}
+        failureRedirect: "/login",
+    })(req, res, next);
+};
 
-const logout = async (req, res, next) => {
-    req.logout((err) => {
+const logout = (req, res, next) => {
+    req.logout(err => {
         if (err) return next(err);
         res.redirect("/");
-    })
-}
+    });
+};
 
-module.exports= {
+module.exports = {
     getUser,
     loginUser,
-    logout
-}
+    logout,
+};
