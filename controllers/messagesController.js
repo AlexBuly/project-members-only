ad0038b03c6e7a3785021ac03fe75497beef7abb
Alex Buly <asbuly@icloud.com>
2025-11-28 20:44:48 -0500

    Add new message function

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,8 +1,27 @@
+const pool = require("../db/pool");
+
 const renderMessageForm = (req, res) => {
+    if (!req.user) return res.redirect("/login")
     res.render("new-message", {title: "Add Message"});
 }
 
+const addMessage = async (req, res) => {
+    if (!req.user) return res.redirect("/login");
+    const {title, text } = req.body;
 
+    try {
+        await pool.query(
+            `
+                INSERT INTO Messages (title, text, user_id, timestamp, author)
+                VALUES ($1, $2, $3, NOW(), $4)
+            `, [title, text, req.user.id, req.user.username]
+        );
+        return res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error saving message");
+    }
+}
 
 module.exports = {
     renderMessageForm,
