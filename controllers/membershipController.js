64190743ef5fd257c623fbb765ae47ab6d3a57c7
Alex Buly <asbuly@icloud.com>
2025-12-01 20:23:22 -0500

    Add membership

--- controllers/membershipController.js
+++ controllers/membershipController.js
@@ -1,8 +1,32 @@
+const pool = require("../db/pool");
+
 const renderMembershipPage = (req, res) => {
      if (!req.user) return res.redirect("/login");
       res.render("membership", {title: "Join"});
 };
 
+const addMembership = async (req, res) => {
+    const { join } = req.body;
+
+    if (join !== "M3SSAGE") {
+        return res.render("membership", {
+            title: "Join"
+        });
+    }
+
+    try {
+        await pool.query(
+            "UPDATE Users SET member = true WHERE id = $1",
+            [req.user.id]
+        );
+        return res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error updating status");
+    }
+}
+
 module.exports = {
-    renderMembershipPage
+    renderMembershipPage,
+    addMembership
 };
--- routes/membershipRouter.js
+++ routes/membershipRouter.js
@@ -3,5 +3,6 @@ const router = Router();
 const membershipController = require("../controllers/membershipController");
 
 router.get("/", membershipController.renderMembershipPage);
+router.post("/", membershipController.addMembership);
 
 module.exports = router;
